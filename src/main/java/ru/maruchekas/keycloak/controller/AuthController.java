d90e843b5db0dd2df52f3965ac44f257747b8369
maruchekas <salken84@gmail.com>
2022-04-07 17:17:18 +0300

    readme added

--- src/main/java/ru/maruchekas/keycloak/controller/AuthController.java
+++ src/main/java/ru/maruchekas/keycloak/controller/AuthController.java
@@ -1,12 +1,14 @@
 package ru.maruchekas.keycloak.controller;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.keycloak.representations.AccessTokenResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.maruchekas.keycloak.api.request.AuthRequest;
 import ru.maruchekas.keycloak.api.request.RefreshTokenRequest;
 import ru.maruchekas.keycloak.service.KeycloakClientService;
