877fce7a70908d59327c4f164802f86b1f17b5b2
maruchekas <salken84@gmail.com>
2022-04-11 15:28:40 +0300

    created tests for authController

--- src/test/java/ru/maruchekas/keycloak/controller/AuthControllerTest.java
+++ src/test/java/ru/maruchekas/keycloak/controller/AuthControllerTest.java
@@ -1,5 +1,6 @@
 package ru.maruchekas.keycloak.controller;
 
+import org.json.JSONObject;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -16,8 +18,6 @@ import ru.maruchekas.keycloak.AbstractTest;
 import ru.maruchekas.keycloak.api.request.AuthRequest;
 import ru.maruchekas.keycloak.api.request.RefreshTokenRequest;
 import ru.maruchekas.keycloak.config.Constants;
-
-import java.util.Random;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -30,7 +30,6 @@ public class AuthControllerTest extends AbstractTest {
     private String username;
     private String password;
     private String clientId;
-    private String realm;
 
     @BeforeEach
     public void setup() {
@@ -38,7 +37,6 @@ public class AuthControllerTest extends AbstractTest {
         username = "myUser";
         password = "password";
         clientId = "mitra-client";
-        realm = "MitraSoftRealm";
     }
 
     @AfterEach
@@ -57,8 +55,7 @@ public class AuthControllerTest extends AbstractTest {
                         .content(mapper.writeValueAsString(authRequest))
                         .accept(MediaType.APPLICATION_JSON))
                 .andDo(MockMvcResultHandlers.print())
-                .andExpect(MockMvcResultMatchers.status().isOk());
-    }
+                .andExpect(MockMvcResultMatchers.status().isOk());    }
 
     @Test
     public void badLoginTest() throws Exception {
@@ -79,11 +76,104 @@ public class AuthControllerTest extends AbstractTest {
 
     @Test
     public void logoutTest() throws Exception {
+
         RefreshTokenRequest tokenRequest = new RefreshTokenRequest();
-        tokenRequest.setRefreshToken(new Random().ints(1, 255)
-                .limit(128)
-                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-                .toString());
+        AuthRequest authRequest = new AuthRequest();
+        authRequest.setUsername(username);
+        authRequest.setPassword(password);
+        authRequest.setClientId(clientId);
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/authenticate")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(authRequest))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+
+        JSONObject jsonObject = new JSONObject(result.getResponse().getContentAsString());
+        tokenRequest.setRefreshToken(jsonObject.getString("refresh_token"));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/authenticate/logout")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(tokenRequest))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    public void refreshTokenTest() throws Exception {
+
+        RefreshTokenRequest tokenRequest = new RefreshTokenRequest();
+        AuthRequest authRequest = new AuthRequest();
+        authRequest.setUsername(username);
+        authRequest.setPassword(password);
+        authRequest.setClientId(clientId);
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/authenticate")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(authRequest))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+
+        JSONObject jsonObject = new JSONObject(result.getResponse().getContentAsString());
+        tokenRequest.setRefreshToken(jsonObject.getString("refresh_token"));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/authenticate/refresh")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(tokenRequest))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    public void badRefreshTokenTest() throws Exception {
+
+        RefreshTokenRequest tokenRequest = new RefreshTokenRequest();
+        AuthRequest authRequest = new AuthRequest();
+        authRequest.setUsername(username);
+        authRequest.setPassword(password);
+        authRequest.setClientId(clientId);
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/authenticate")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(authRequest))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+
+        JSONObject jsonObject = new JSONObject(result.getResponse().getContentAsString());
+        tokenRequest.setRefreshToken(jsonObject.getString("refresh_token") + "a");
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/authenticate/refresh")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(tokenRequest))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message")
+                        .value(Constants.INVALID_REFRESH_TOKEN.getMessage()))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    @Test
+    public void badLogoutTest() throws Exception {
+
+        RefreshTokenRequest tokenRequest = new RefreshTokenRequest();
+        AuthRequest authRequest = new AuthRequest();
+        authRequest.setUsername(username);
+        authRequest.setPassword(password);
+        authRequest.setClientId(clientId);
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/authenticate")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(authRequest))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+
+        JSONObject jsonObject = new JSONObject(result.getResponse().getContentAsString());
+        tokenRequest.setRefreshToken(jsonObject.getString("refresh_token") + "a");
 
         mockMvc.perform(MockMvcRequestBuilders
                         .post("/api/authenticate/logout")
